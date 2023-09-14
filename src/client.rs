30dbd7fdc57db09d995e6e7779501d5a28d20450
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2023-09-13 19:59:28 -0500

    Derive trait Clone for GroupmeClient, change constructor to accept String"

--- src/client.rs
+++ src/client.rs
@@ -10,6 +10,7 @@ pub type BotId = str;
 
 const BASE_URL: &'static str = "https://api.groupme.com/v3";
 
+#[derive(Clone)]
 pub struct GroupmeClient {
     client: Client,
     bot_id: String,
@@ -17,20 +18,21 @@ pub struct GroupmeClient {
 
 #[derive(Debug, Error)]
 pub enum PostBotMsgError {
-    MsgBodyRejected,
     ReqBodySerError,
 }
 
 impl std::fmt::Display for PostBotMsgError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        match self {
+            Self::ReqBodySerError => write!(f, "Content could not be parsed into JSON")
+        }
     }
 }
 
 impl GroupmeClient {
-    pub fn new(bot_id: &BotId) -> Self {
+    pub fn new(bot_id: String) -> Self {
         GroupmeClient {
-            bot_id: bot_id.to_string(),
+            bot_id,
             client: ClientBuilder::new()
                 .https_only(true)
                 .timeout(Duration::new(10, 0))
@@ -39,6 +41,10 @@ impl GroupmeClient {
         }
     }
 
+    /// Post a message with `content` using the calling client's `bot_id` as the context.
+    /// Returns unit type if the content was accepted by the server.
+    /// Returns an error if the content cannot be parsed into JSON,
+    /// or if the server responds with a status that is not '200'.
     pub async fn post_bot_message(&self, content: &str) -> Result<(), Box<dyn Error>> {
         let url = format!("{}/bots/post", BASE_URL);
 
